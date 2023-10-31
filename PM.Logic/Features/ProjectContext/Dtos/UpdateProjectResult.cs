7d82885cd61b1e57d00f24964ed9c2a1c6bcbd4d
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 15:43:51 +0600

    fixed

--- PM.Logic/Features/ProjectContext/Dtos/UpdateProjectResult.cs
+++ PM.Logic/Features/ProjectContext/Dtos/UpdateProjectResult.cs
@@ -1,13 +1,32 @@
-﻿using PM.Domain.Common.Enums;
+﻿using Mapster;
+using PM.Domain.Common.Enums;
+using PM.Domain.Entities;
 
 namespace PM.Application.Features.ProjectContext.Dtos;
 
-public sealed record UpdateProjectResult(
-     int Id,
-     string Name,
-     int CustomerCompanyId,
-     int ExecutorCompanyId,
-     int ManagerId,
-     DateTime StartDate,
-     DateTime EndDate,
-     ProjectPriority Priority);
+public sealed class UpdateProjectResult : IRegister
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int CustomerCompanyId { get; set; }
+
+    public int ExecutorCompanyId { get; set; }
+
+    public int ManagerId { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public ProjectPriority Priority { get; set; }
+
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Project, UpdateProjectResult>()
+            .Map(dest => dest.CustomerCompanyId, src => src.CustomerCompany.Id)
+            .Map(dest => dest.ExecutorCompanyId, src => src.ExecutorCompany.Id)
+            .Map(dest => dest.ManagerId, src => src.Manager.Id);
+    }
+}
