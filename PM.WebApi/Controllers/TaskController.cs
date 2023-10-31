0701f38536b693db3ff6d26110a0974695440df9
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 20:28:27 +0600

    Added Task Create method

--- PM.WebApi/Controllers/TaskController.cs
+++ PM.WebApi/Controllers/TaskController.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PM.Domain.Common.Enums;
+using PM.Application.Features.TaskContext.Commands.CreateTask;
+using PM.Contracts.TaskContracts.Requests;
+using PM.Contracts.TaskContracts.Responses;
 
 namespace PM.WebApi.Controllers;
 
 public class TaskController : BaseController
 {
-    [HttpGet]
-    public async Task<IActionResult> GetTasksAsync()
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateTaskRequest), StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateTaskAsync(
+        CreateTaskRequest request,
+        CancellationToken cancellationToken)
     {
-        return Ok(ProjectPriority.Low);
+        var command = Mapper.Map<CreateTaskCommand>(request);
+        var result = await Mediator.Send(command, cancellationToken);
+
+        return result.Match(
+            result => Ok(Mapper.Map<CreateTaskResponse>(result)),
+            errors => Problem(errors));
+    }
+
+
+    [HttpPut]
+    [ProducesResponseType(typeof(CreateTaskRequest), StatusCodes.Status200OK)]
+    public async Task<IActionResult> UpdateTaskAsync(
+        UpdateTaskRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = Mapper.Map<UpdateTaskCommand>(request);
+        var result = await Mediator.Send(command, cancellationToken);
+
+        return result.Match(
+            result => Ok(Mapper.Map<CreateTaskResponse>(result)),
+            errors => Problem(errors));
     }
 }
