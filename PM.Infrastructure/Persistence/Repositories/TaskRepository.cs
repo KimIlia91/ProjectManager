9b0d4ca6b3618c1a103efb1b3a86f9ef4ad184f6
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 20:19:18 +0600

    Added Task repository

---
+++ PM.Infrastructure/Persistence/Repositories/TaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using PM.Application.Common.Interfaces.IRepositories;
+using Task = PM.Domain.Entities.Task;
+
+namespace PM.Infrastructure.Persistence.Repositories;
+
+public sealed class TaskRepository : BaseRepository<Task>, ITaskRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public TaskRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
