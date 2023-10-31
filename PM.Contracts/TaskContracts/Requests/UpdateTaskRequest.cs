b5c4aace0c1927877b6ad31c197e076b2ef7922c
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 20:36:06 +0600

    Added update task request

---
+++ PM.Contracts/TaskContracts/Requests/UpdateTaskRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Contracts.TaskContracts.Requests;
+
+public sealed record UpdateTaskRequest(
+    int Id,
+    string Name,
+    int AuthorId,
+    int ExecutorId,
+    string? Commnet,
+    int Status,
+    int Priority);
