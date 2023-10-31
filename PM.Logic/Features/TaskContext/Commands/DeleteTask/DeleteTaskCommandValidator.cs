81f8663969459a06583789efcaaa76071146bae8
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 21:49:53 +0600

    Added Validator

---
+++ PM.Logic/Features/TaskContext/Commands/DeleteTask/DeleteTaskCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace PM.Application.Features.TaskContext.Commands.DeleteTask;
+
+internal sealed class DeleteTaskCommandValidator : AbstractValidator<DeleteTaskCommand>
+{
+    public DeleteTaskCommandValidator()
+    {
+        RuleFor(command => command.Id)
+            .NotEmpty();
+    }
+}
