c8bd16955f956bc90b311a3e08f3114848d8742c
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-10-31 20:35:32 +0600

    Added create task request

---
+++ PM.Contracts/TaskContracts/Requests/CreateTaskRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Contracts.TaskContracts.Requests;
+
+public sealed record CreateTaskRequest(
+    int ProjectId,
+    string Name,
+    int AuthorId,
+    int ExecutorId,
+    string? Commnet,
+    int Status,
+    int Priority);
