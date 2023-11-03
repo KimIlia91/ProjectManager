e95796caefe7904a75fe00ba648186f7c6491335
paterfamelias@gmail.com <paterfamelias@gmail.com>
2023-11-03 21:13:27 +0600

    Added

--- PM.Infrastructure/DependencyInjection.cs
+++ PM.Infrastructure/DependencyInjection.cs
@@ -2,25 +2,19 @@
 using Microsoft.Extensions.DependencyInjection;
 using PM.Infrastructure.Persistence;
 using PM.Infrastructure.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PM.Infrastructure
+namespace PM.Infrastructure;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructure(
-            this IServiceCollection services,
-            IConfiguration configuration)
-        {
-            services.AddPesistence(configuration);
-            services.AddServices();
-            services.AddAuth(configuration);
+        services.AddPesistence(configuration);
+        services.AddServices();
+        services.AddAuth(configuration);
 
-            return services;
-        }
+        return services;
     }
 }
